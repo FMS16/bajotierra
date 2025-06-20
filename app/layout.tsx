2f9a7158eeee5641fe8dfa0bfee9aa88d40147a1
Fabricio <martinsecondo16@gmail.com>
2025-06-19 22:40:31 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Be_Vietnam_Pro} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const vietnam = Be_Vietnam_Pro({
+  variable: "--font-vietnam",
+  weight: 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
