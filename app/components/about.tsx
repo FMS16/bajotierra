5cd9d49ea8deb6f50cb05b107f2311e9dff40bbe
FSS17 <martinsecondo16@gmail.com>
2025-06-20 10:05:24 -0300

    updates

---
+++ app/components/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full h-full mb-16'>
+        <div className='w-[90%] md:container mx-auto py-10 px-7 md:px-16 md:py-16 gradient-custom-1 -mt-10 rounded-3xl'>
+            <h1 className='text-4xl text-center md:text-5xl text-white opacity-30'>&Eacute;rase una vez...</h1>
+            <p className='text-center text-base md:text-lg text-white opacity-60 leading-8 mt-10 md:mt-16'>“Bajo Tierra” cuenta la historia de Juan, un joven de 25 años muy sensible que, desde niño, está acostumbrado a reprimir su tristeza. Luego de la trágica muerte de sus padres, Juan regresa por primera vez a la casa familiar. Allí, en ese entorno cargado de recuerdos y emociones contenidas, se ve obligado a enfrentarse a un monstruo. Para lograr que desaparezca, tendrá que enfrentarse a sí mismo y a aquello que ha callado durante años.</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
