56ad084fc0bbd90e7921c56df03d1f3b67cd8248
FSS17 <martinsecondo16@gmail.com>
2025-06-20 10:01:37 -0300

    componentes para la web, historia de la pelicula

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Banner from "./components/banner";
+import OurTeam from "./components/ourTeam";
+import About from "./components/about";
+import Gallery from "./components/gallery";
 
 export default function Home() {
   return (
     <div>
-        <Banner />
+      <Banner />
+      <About />
+      <Gallery />
     </div>
   );
 }
